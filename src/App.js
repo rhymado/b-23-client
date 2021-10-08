15aa108de2733788c1a30a80038d2e48271b057a
Fakhri <mightydhiyamatra@yahoo.co.id>
2021-10-08 11:35:38 +0700

    sesi pagi'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo } from "react";
+import { io } from "socket.io-client";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const socket = useMemo(() => io("http://localhost:8000"), []);
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const body = {
+      email,
+      password,
+    };
+    if (socket)
+      return socket.emit("send_login_form", body, (response) => {
+        console.log(response.msg);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <section className="content">
+        <form onSubmit={onSubmitHandler} className="login-form">
+          <input
+            type="text"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </main>
   );
 }
 
